a013ed2e5ab0971b47a3324c625c9b1508c5ff5e
RafaelHub <raffiscoffe@gmail.com>
2025-04-05 22:34:30 +0000

    batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main()
-{
+{   //  Matriz do tabuleiro de batalha naval
     int tabuleiro[10][10] = {
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
@@ -15,26 +15,26 @@ int main()
         {0,0,0,0,0,0,0,0,0,0}
     };
 
-    int navio1[3] = {3,3,3};
-    int navio2[3] = {3,3,3};
+    int navio1[3] = {3,3,3}; // array do navio-1
+    int navio2[3] = {3,3,3}; // array do navio-2
 
-    int LinhaV = 4;
-    int ColunaV = 6;
+    int LinhaV = 4; // Posição vertical do navio-1
+    int ColunaV = 6; // posição vertical do navio-1
 
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < 3; i++) // loop, para adicionar ao tabuleiro o navio-1
     {
         tabuleiro[LinhaV][ColunaV + i] = navio1[i];
     };
 
-    int LinhaH = 7;
-    int ColunaH = 4;
+    int LinhaH = 7; // posição horizontal do navio-2
+    int ColunaH = 4; // posição horizontal do navio-2
 
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < 3; i++) // loop, para adicionar ao tabuleiro o navio-2
     {
         tabuleiro[LinhaH + i][ColunaH] = navio2[i];
     };
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 10; i++) // loops aninhados, para exibição completa da matriz, e seus navios
     {
         for(int j = 0; j < 10;j++)
         {
@@ -42,4 +42,6 @@ int main()
         }
         printf("\n");
     }
+
+    return 0;
 }
